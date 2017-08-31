f70e4439c4a37029bdaf4acefdc87b24473dce3b
DESKTOP-EF2MDG9\morino-kazuya <kazuya.morino@exmotion.co.jp>
2017-08-31 15:31:53 +0900

    ・上げ忘れのprojファイルを上げる ・測定中のデータサイズの表示を3桁区切りにした ・使ってない変数を消した

--- CommCheck/Program.cs
+++ CommCheck/Program.cs
@@ -10,11 +10,12 @@ namespace CommCheck
             {
                 Console.WriteLine("http+jsonで、エンコード→通信→でコードでどれくらいの時間がかかるのかを測定します");
  
-                #if false
+                #define USE_BSON
+                #if USE_BSON
                 var examainBase = ExaminBuilder.Instance()
                     .SetPostTest()
                     .SetBsonUsage()
-                    .SetDataSizeArray(new[] {5 * 1000, 1000 * 1000,})
+                    .SetDataSizeArray(new[] {5 * 1000, 1000 * 1000, 500 * 1000 * 1000})
                     .SetCommIntervalMillSec(new[]{0, 10, 20})
                     .SetExaminNum(20000)
                     .SetThreadNum(new[]{1, 5})
@@ -22,7 +23,7 @@ namespace CommCheck
                 #else
                 var examainBase = ExaminBuilder.Instance()
                     .SetPostTest()
-                    .SetDataSizeArray(new[] {5 * 1000, 1000 * 1000, 500 * 1000 * 1000, 2000 * 1000 * 1000})
+                    .SetDataSizeArray(new[] {5 * 1000, 1000 * 1000, 500 * 1000 * 1000})
                     .SetCommIntervalMillSec(new[]{0, 10, 20})
                     .SetExaminNum(20000)
                     .SetThreadNum(new[]{1, 5})
